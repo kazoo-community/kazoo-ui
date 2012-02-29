296950ff8aa4e01a98f4d6aeb223dff32317ddc2
Jean-Roch Maitre <68488@supinfo.com>
2012-02-29 01:19:26 -0800

    WINKSTART-669: Work in progress for Support tool. (CDRs module)

--- whapps/voip/cdr/cdr.js
+++ whapps/voip/cdr/cdr.js
@@ -43,6 +43,8 @@ function(args) {
 },
 {
     list_by_date: function(start_date, end_date) {
+        var THIS = this;
+
 		function noData(data){
 			if(data == null || data == undefined){
 				data = '-';
@@ -59,23 +61,26 @@ function(args) {
                 created_to: end_date
             },
             function(reply) {
+                var cdr_id, caller_id_name, caller_id_number, callee_id_name, callee_id_number, hangup_cause, duration, seconds, minutes, date, hours, month, year, day, humanDate, humanTime, humanFullDate, web_browser_id;
+
                 $.each(reply.data, function() {
-                    var cdr_id = this.cid || this.id;
-                    var caller_id_name = this.caller_id_name;
-                    var caller_id_number = this.caller_id_number;
-                    var callee_id_name = this.callee_id_name;
-                    var callee_id_number = this.callee_id_number;
-                    //var duration = this.billing_seconds;
-                    var duration = this.duration_seconds;
-                    var seconds = duration % 60;
-                    var minutes = (duration-seconds) / 60;
+                    cdr_id = this.cid || this.id;
+                    caller_id_name = this.caller_id_name;
+                    caller_id_number = this.caller_id_number;
+                    callee_id_name = this.callee_id_name;
+                    callee_id_number = this.callee_id_number;
+                    hangup_cause = this.hangup_cause;
+                    duration = this.duration_seconds;
+                    seconds = duration % 60;
+                    minutes = (duration-seconds) / 60;
+
+                     hours = (duration - (minutes*60) - seconds) / 3600;
+                    if(hours < 10) {
+                        hours = '0' + hours;
+                    }
 
                     if(minutes > 59) {
                         minutes -= 60;
-                    }
-                    var hours = (duration - (minutes*60) - seconds) / 3600;
-                    if(hours < 10) {
-                        hours = '0' + hours;
                     }
                     if(minutes < 10) {
                         minutes = '0' + minutes;
@@ -84,14 +89,15 @@ function(args) {
                         seconds = '0' + seconds;
                     }
                     duration = hours+':'+minutes+':'+seconds;
-                    var date = new Date((this.timestamp - 62167219200)*1000);
-                    var month = date.getMonth() +1;
-                    var year = date.getFullYear();
-                    var day = date.getDate();
-                    var humanDate = month+'/'+day+'/'+year;
-                    var humanTime = date.toLocaleTimeString();
+                    date = new Date((this.timestamp - 62167219200)*1000);
+                    month = date.getMonth() +1;
+                    year = date.getFullYear();
+                    day = date.getDate();
+                    humanDate = month+'/'+day+'/'+year;
+                    humanTime = date.toLocaleTimeString();
+                    web_browser_id = THIS.parse_cdr_id(cdr_id);
 
-                    var humanFullDate = humanDate + ' ' + humanTime;
+                    humanFullDate = humanDate + ' ' + humanTime;
 
                     if(caller_id_name && caller_id_number && callee_id_name && callee_id_number){
                         winkstart.table.cdr.fnAddData([
@@ -100,12 +106,18 @@ function(args) {
                             noData(callee_id_name),
                             noData(callee_id_number),
                             noData(duration),
+                            noData(hangup_cause),
+                            '<a href="http://www.google.com/'+web_browser_id +'" target="_blank">Log</a>',
                             noData(humanFullDate)
                         ]);
                     }
                 });
             }
         );
+    },
+
+    parse_cdr_id: function(cdr_id) {
+        return cdr_id.substr(0,1) + '/' + cdr_id.substr(1,1) + '/' + cdr_id.substr(2,1) + '/' + cdr_id;
     },
 
     init_table: function(parent) {
@@ -133,12 +145,21 @@ function(args) {
                 'sWidth': '80px'
             },
             {
+                'sTitle': 'Hangup Cause',
+                'sWidth': '150px'
+            },
+            {
+                'sTitle': 'Logs',
+                'sWidth': '150px'
+            },
+            {
                 'sTitle': 'Date'
             }
 		];
 
 		winkstart.table.create('cdr', $('#cdr-grid', cdr_html), columns, {}, {
-			sDom: '<"date">frtlip'
+			sDom: '<"date">frtlip',
+            aaSorting: [[7, 'desc']]
 		});
     },
 
@@ -150,66 +171,137 @@ function(args) {
 
         THIS.init_table(cdr_html);
 
-		var functionFilter = function(oSettings, aData, iDataIndex) {
-			var dateMin = '',
-			    dateMax = '';
-
-			if($('#startDate', cdr_html).val() != '') {
-				dateMin = new Date($('#startDate', cdr_html).val());
-			}
-			if($('#endDate', cdr_html).val() != '') {
-				dateMax = new Date($('#endDate', cdr_html).val());
-			}
-			var dateVersion = new Date(aData[5]);
-			if ( dateMin == "" && dateMax == "" )
-			{
-				return true;
-			}
-			else if ( dateMin == "" && dateVersion <= dateMax )
-			{
-				return true;
-			}
-			else if ( dateMin <= dateVersion && "" == dateMax )
-			{
-				return true;
-			}
-			else if ( dateMin <= dateVersion && dateVersion <= dateMax )
-			{
-				return true;
-			}
-			return false;
-		};
-
 		$.fn.dataTableExt.afnFiltering.pop();
 
-		$('div.date', cdr_html).html('Start Date: <input id="startDate" type="text"/>&nbsp;&nbsp;End Date: <input id="endDate" type="text"/>&nbsp;&nbsp;&nbsp;&nbsp;<a class="button-search fancy_button blue" id="searchLink" href="javascript:void(0);">Filter</a>');
-
-		$('#startDate, #endDate', cdr_html).focus(function() {
-			$('div.date input', cdr_html).removeClass('focusField');
-			$(this).addClass('focusField');
-		});
+		$('div.date', cdr_html).html('Start Date: <input id="startDate" readonly="readonly" type="text"/>&nbsp;&nbsp;End Date: <input id="endDate" readonly="readonly" type="text"/>&nbsp;&nbsp;&nbsp;&nbsp;<a class="button-search fancy_button blue" id="searchLink" href="javascript:void(0);">Filter</a>');
 
 		$('#searchLink', cdr_html).click(function() {
-//			$.fn.dataTableExt.afnFiltering.push(functionFilter);
-//			winkstart.table.cdr.fnDraw();
+            var start_date = $('#startDate', cdr_html).val(),
+                end_date = $('#endDate', cdr_html).val(),
+                regex = /^(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d$/;
 
             winkstart.table.cdr.fnClearTable();
-            var start_date = $('#startDate', cdr_html).val(),
-                end_date = $('#endDate', cdr_html).val();
 
-            //todo regex to match date
-            var start_date_sec = (new Date(start_date).getTime()/1000) + 62167219200,
-                end_date_sec = (new Date(end_date).getTime()/1000) + 62167219200
+            if(start_date.match(regex) && end_date.match(regex)) {
+                var start_date_sec = (new Date(start_date).getTime()/1000) + 62167219200,
+                    end_date_sec = (new Date(end_date).getTime()/1000) + 62167219200;
 
-            THIS.list_by_date(start_date_sec, end_date_sec);
+                if((end_date_sec - start_date_sec) <= (7*24*60*60)) {
+                    THIS.list_by_date(start_date_sec, end_date_sec);
+                }
+                else {
+                    winkstart.alert('The range is bigger than 7 days, please correct it.');
+                }
+            }
+            else {
+                winkstart.alert('Dates in the filter are not in the proper format (mm/dd/yyyy)');
+            }
 		});
 
-		$('#startDate', cdr_html).datepicker();
-		$('#endDate', cdr_html).datepicker();
+        THIS.init_datepicker(cdr_html);
 
-        var current_time = Math.floor(new Date().getTime()/1000) + 62167219200,
-            prev_week_time = current_time - (7*24*60*60);
+        var tomorrow = new Date(THIS.to_string_date(new Date()));
+        tomorrow.setDate(tomorrow.getDate() + 1);
 
-        THIS.list_by_date(prev_week_time, current_time);
-	}
+        var end_date = Math.floor(tomorrow.getTime()/1000) + 62167219200,
+            start_date = end_date - (7*24*60*60);
+
+        THIS.list_by_date(start_date, end_date);
+	},
+
+    init_datepicker: function(parent) {
+        var THIS = this,
+            cdr_html = parent,
+            $start_date = $('#startDate', cdr_html),
+            $end_date = $('#endDate', cdr_html),
+            start_date = new Date(),
+            end_date,
+            tomorrow = new Date();
+
+        tomorrow.setDate(tomorrow.getDate() + 1);
+
+		$('#startDate, #endDate', cdr_html).datepicker(
+            {
+                beforeShow: customRange,
+                onSelect: customSelect
+            }
+        );
+
+        start_date.setDate(tomorrow.getDate() - 7);
+        end_date = tomorrow;
+
+        $start_date.datepicker('setDate', start_date);
+        $end_date.datepicker('setDate', end_date);
+
+        function customSelect(dateText, input) {
+            var date_min,
+                date_max;
+
+            if(input.id == 'startDate') {
+                date_min = $start_date.datepicker('getDate');
+                if($end_date.datepicker('getDate') == null) {
+                    date_max = date_min;
+                    date_max.setDate(date_min.getDate() + 7);
+                    $end_date.val(THIS.to_string_date(date_max));
+                }
+                else {
+                    date_max = $end_date.datepicker('getDate');
+                    if((date_max > (new Date(date_min).setDate(date_min.getDate() + 7)) || (date_max <= date_min))) {
+                        date_max = date_min;
+                        date_max.setDate(date_max.getDate() + 7);
+                        date_max > tomorrow ? date_max = tomorrow : true;
+                        $end_date.val(THIS.to_string_date(date_max));
+                    }
+                }
+            }
+            else if(input.id == 'endDate') {
+                if($start_date.datepicker('getDate') == null) {
+                    date_min = $end_date.datepicker('getDate');
+                    date_min.setDate(date_min.getDate() - 7);
+                    $start_date.val(THIS.to_string_date(date_min));
+                }
+            }
+        };
+
+        function customRange(input) {
+            var date_min = new Date(2011, 0, 0),
+                date_max;
+
+            if (input.id == 'endDate')
+            {
+                date_max = tomorrow;
+                if ($start_date.datepicker('getDate') != null)
+                {
+                    date_min = $start_date.datepicker('getDate');
+                    /* Range of 1 day minimum */
+                    date_min.setDate(date_min.getDate() + 1);
+                    date_max = $start_date.datepicker('getDate');
+                    date_max.setDate(date_max.getDate() + 7);
+
+                    if(date_max > tomorrow) {
+                        date_max = tomorrow;
+                    }
+                }
+            }
+            else if (input.id == 'startDate') {
+                date_max = new Date();
+            }
+
+            return {
+                minDate: date_min,
+                maxDate: date_max
+            };
+        }
+    },
+
+    to_string_date: function(date) {
+       var day = date.getDate(),
+                month = date.getMonth()+1,
+                year = date.getFullYear();
+
+            day < 10 ? day = '0' + day : true;
+            month < 10 ? month = '0' + month : true;
+
+            return month+'/'+day+'/'+year;
+    }
 });
