64dcaa858eb123c54ea72b410609d4f6a6e42692
Jean-Roch Maitre <68488@supinfo.com>
2012-03-19 18:20:25 -0700

    WINKSTART-711: Added Numbers to the validation of user first name and last name.

--- whapps/voip/user/user.js
+++ whapps/voip/user/user.js
@@ -18,8 +18,8 @@ winkstart.module('voip', 'user', {
         },
 
         validation : [
-                { name: '#first_name',                regex: /^[a-zA-Z\s\-\']+$/ },
-                { name: '#last_name',                 regex: /^[a-zA-Z\s\-\']+$/ },
+                { name: '#first_name',                regex: /^[0-9a-zA-Z\s\-\']+$/ },
+                { name: '#last_name',                 regex: /^[0-9a-zA-Z\s\-\']+$/ },
                 { name: '#username',                  regex: /^[a-zA-Z0-9\_\-]{3,16}$/ },
                 { name: '#email',                     regex: /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/ },
                 { name: '#caller_id_number_internal', regex: /^[\+]?[0-9\s\-\.\(\)]*$/ },
