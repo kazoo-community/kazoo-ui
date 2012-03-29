253c42be56d3c0ff3025268e443db7e53c268cbd
Jean-Roch Maitre <68488@supinfo.com>
2012-03-28 19:23:17 -0700

    WINKSTART-750: Call-Center should not be displayed in v1.51.

--- whapps/voip/voip.js
+++ whapps/voip/voip.js
@@ -65,7 +65,7 @@ winkstart.module('voip', 'voip', {
             'timeofday': false,
             'featurecode': false,
             'cdr': false,
-            'queue': false,
+            /*'queue': false,*/
             'directory': false
         },
 
