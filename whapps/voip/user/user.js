20777ba420d8c5cbc5dca08bcd7a14fe869cd327
Jean-Roch Maitre <68488@supinfo.com>
2012-04-25 16:56:02 -0700

    WINKSTART-779: Made a fix in the front-end for the empty directory array in User

--- whapps/voip/user/user.js
+++ whapps/voip/user/user.js
@@ -683,6 +683,10 @@ winkstart.module('voip', 'user', {
         },
 
         normalize_data: function(data) {
+            if($.isArray(data.directories)) {
+                data.directories = {};
+            }
+
             $.each(data.caller_id, function(key, val) {
                 $.each(val, function(_key, _val) {
                     if(_val == '') {
