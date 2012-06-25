d4b611c35b436deb727dc66af1680da1902598d1
Jean-Roch Maitre <68488@supinfo.com>
2012-06-25 16:21:01 -0700

    WINKSTART-897: Remove queues from the UI.

--- whapps/voip/voip.js
+++ whapps/voip/voip.js
@@ -86,7 +86,7 @@ winkstart.module('voip', 'voip', {
             'timeofday': false,
             'featurecode': false,
             'cdr': false,
-            'queue': false,
+            //'queue': false,
             'directory': false
         },
 
