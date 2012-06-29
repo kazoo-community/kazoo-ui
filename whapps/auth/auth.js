4945caf90ad1bcca6b660e75de176333e849d440
Peter Defebvre <peter@2600hz.com>
2012-06-29 12:03:19 -0700

    Fix invite code if empty

--- whapps/auth/auth.js
+++ whapps/auth/auth.js
@@ -363,30 +363,32 @@ winkstart.module('auth', 'auth',
                 e.preventDefault();
                 var code = $('input#code', code_html).val();
 
-
-                winkstart.request('auth.invite_code', {
-                        api_url: winkstart.apps.auth.api_url,
-                        invite_code: code,
-                    },
-                    function(_data, status) {
-                        winkstart.publish('onboard.register', {
-                            invite_code: code
-                        });
-                    },
-                    function(_data, status) {
-                        switch(_data['error']) {
-                            case '404':
-                                winkstart.alert('error', 'Invalid invite code !');
-                                break;
-                            case '410':
-                                winkstart.alert('error', 'Invite code already used !');
-                                break;
-                            default:
-                                winkstart.alert('error', winkstart.print_r(_data));
-                                break;
+                if(code != "" && code != null) {
+                    winkstart.request('auth.invite_code', {
+                            api_url: winkstart.apps.auth.api_url,
+                            invite_code: code,
+                        },
+                        function(_data, status) {
+                            winkstart.publish('onboard.register', {
+                                invite_code: code
+                            });
+                        },
+                        function(_data, status) {
+                            switch(_data['error']) {
+                                case '404':
+                                    winkstart.alert('error', 'Invalid invite code !');
+                                    break;
+                                case '410':
+                                    winkstart.alert('error', 'Invite code already used !');
+                                    break;
+                                default:
+                                    winkstart.alert('error', '<p>An error occurred</p>' + winkstart.print_r(_data));
+                                    break;
+                            }
                         }
-                    }
-                );
+                    );
+                }
+                
             });
 
             $('a.recover_password', contentDiv).click(function(e) {
