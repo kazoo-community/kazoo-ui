a1527d36447bdd34fa3b75a406c50959b6994159
Jean-Roch Maitre <68488@supinfo.com>
2012-07-25 21:17:58 -0700

    WINKSTART-981: Fixed a bug when template was not displayed for whitelabel
    Conflicts:
    
    	whapps/accounts/accounts_manager/accounts_manager.js

--- whapps/accounts/accounts_manager/accounts_manager.js
+++ whapps/accounts/accounts_manager/accounts_manager.js
@@ -303,7 +303,7 @@ winkstart.module('accounts', 'accounts_manager', {
                                 api_url: winkstart.apps['accounts'].api_url
                             },
                             function(_data_wl, status) {
-                                defaults.field_data.whitelabel = _data_wl.data;
+                                defaults.field_data.whitelabel = $.extend(true, defaults.field_data.whitelabel, _data_wl.data);
                                 defaults.field_data.whitelabel.logo_url = winkstart.apps['accounts'].api_url + '/accounts/'+data.id+'/whitelabel/logo?auth_token='+winkstart.apps['accounts'].auth_token;
 
                                 render();
