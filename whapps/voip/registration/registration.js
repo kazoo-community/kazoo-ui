1cca233c5aa87ec236c3f079fc21cb370e677546
Jean-Roch Maitre <68488@supinfo.com>
2012-08-01 18:25:55 -0700

    WINKSTART-1005: Display the Contact IP instead of the Proxy IP in the IP Field of the Registration module.

--- whapps/voip/registration/registration.js
+++ whapps/voip/registration/registration.js
@@ -90,6 +90,7 @@ winkstart.module('voip', 'registration',
                         stringToDisplay += '<br/>From-Host: ' + this.from_host;
                         stringToDisplay += '<br/>From-User: ' + this.from_user;
                         stringToDisplay += '<br/>Network-IP: ' + this.network_ip;
+                        stringToDisplay += '<br/>Contact-IP: ' + this.contact_ip;
                         stringToDisplay += '<br/>Network-Port: ' + this.network_port;
                         stringToDisplay += '<br/>Presence-Hosts: ' + this.presence_hosts;
                         stringToDisplay += '<br/>Profile-Name: ' + this.profile_name;
@@ -103,7 +104,7 @@ winkstart.module('voip', 'registration',
                         stringToDisplay += '<br/>Username: ' + this.username;
                         stringToDisplay += '<br/>Date: ' + humanTime;
 
-                        tab_data.push([this.username, this.network_ip, this.network_port, humanTime, stringToDisplay]);
+                        tab_data.push([this.username, this.contact_ip, this.network_port, humanTime, stringToDisplay]);
                     });
 
                     winkstart.table.registration.fnAddData(tab_data);
