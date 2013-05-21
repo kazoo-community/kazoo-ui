cb5445d9034da0a35334d2651f74cb78efd0d778
Jean-Roch Maitre <jr@2600hz.com>
2013-05-20 22:51:49 -0700

    KAZOOUI-254 : Fixed an issue when braintree is not configured (account wasn't displayed)

--- whapps/accounts/accounts_manager/accounts_manager.js
+++ whapps/accounts/accounts_manager/accounts_manager.js
@@ -297,6 +297,9 @@ winkstart.module('accounts', 'accounts_manager', {
                                     defaults.credits = _data_c.data;
 
                                     callback(null, _data_c);
+                                },
+                                function(data, status) {
+                                    callback(null, {});
                                 }
                             );
                         }
