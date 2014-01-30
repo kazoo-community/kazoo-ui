cd19dd9d48319f034f136c753befccd8de2cff39
Anton Puzanov <anton.puzanov@gdsln.com>
2014-01-30 13:25:21 +0700

    updated translation

--- config/load.js
+++ config/load.js
@@ -7,5 +7,6 @@ if (language == "auto"){
 window.language = language;
 $LAB.script('config/lang/' + window.language + '.js')
 		.script('whapps/auth/auth/lang/' + window.language + '.js')
+		.script('whapps/voip/voip/lang/' + window.language + '.js')
 		.script('config/config.js').wait();
 
---
+++ whapps/voip/voip/lang/ru.js
@@ -0,0 +1,18 @@
+window.translate['voip'] = {
+	welcome_to_your_voip: "Welcome to your VoIP Services Dashboard!",
+	account_info: "Account Info",
+	numbers: "Numbers",
+	account: "Account:",
+	account_id: "Account ID:",
+	account_realm: "Account Realm:",
+	support_contact: "Support Contact:",
+	carrier: "Carrier:",
+	direct_sub_accounts: "Direct sub-accounts:",
+	sub_accounts: "Sub-accounts:",
+	devices: "Devices",
+	users: "Users",
+	voicemail_boxes: "Voicemail Boxes",
+	conferences: "Conferences",
+	active_feature_codes: "Active feature codes",
+	callflows: "Callflows"
+};
