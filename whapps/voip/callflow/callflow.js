4f15bdc78504398c71d38f8a2576d9c566d0a969
SIPLABS Communications <info@siplabs.ru>
2014-02-03 11:53:44 +0700

    More language updates

--- whapps/voip/callflow/callflow.js
+++ whapps/voip/callflow/callflow.js
@@ -1039,11 +1039,6 @@ winkstart.module('voip', 'callflow', {
                 target,
                 tools;
 
-            /* Don't add categories here, this is just a hack to order the list on the right */
-            THIS.categories = {
-                _t('callflow', 'basic'): [],
-                _t('callflow', 'advanced'): []
-            };
 
             $.each(THIS.actions, function(i, data) {
                 if('category' in data) {
@@ -1059,6 +1054,12 @@ winkstart.module('voip', 'callflow', {
 					return window.translate['callflow'][param];
 				}
             });
+
+            /* Don't add categories here, this is just a hack to order the list on the right */
+            THIS.categories = {
+                _t('basic'): [],
+                _t('advanced'): []
+            };
 
             $('.content', tools).hide();
 
