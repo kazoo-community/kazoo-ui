c9020e46632c6e794f298accafafd2e7d95b185c
Maxime Roux <max@2600hz.com>
2014-07-01 11:02:59 -0700

    KAZOO-2677: Added validation to the 'send from' email field in the notifications templates tab of accounts

--- whapps/accounts/accounts_manager/accounts_manager.js
+++ whapps/accounts/accounts_manager/accounts_manager.js
@@ -21,7 +21,7 @@ winkstart.module('accounts', 'accounts_manager', {
 		validation: [
 				{ name: '#vm_to_email_support_number',   regex: /^[\+]?[0-9\s\-\x\(\)]*$/ },
 				{ name: '#vm_to_email_support_email',    regex: _t('accounts', 'vm_to_email_support_email_regex') },
-				{ name: '#vm_to_email_send_from',        regex: /^.*$/ },
+				{ name: '#vm_to_email_send_from',        regex: _t('accounts', 'vm_to_email_support_email_regex') },
 				{ name: '#vm_to_email_service_url',      regex: /^.*$/ },
 				{ name: '#vm_to_email_service_provider', regex: /^.*$/ },
 				{ name: '#vm_to_email_service_name',     regex: /^.*$/ },
