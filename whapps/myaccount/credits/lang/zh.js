a46c6bcf5b4f30e6ab205d5b2d10c2f27bdeef1f
xie wei <jxiewei@163.com>
2014-08-26 17:00:10 +0800

    Added zh_CN language support.

---
+++ whapps/myaccount/credits/lang/zh.js
@@ -0,0 +1,22 @@
+window.translate['credits'] = {
+per_minute_calling: "时长式呼叫",
+flat_rate_calling: "租赁式呼叫",
+pay_per_minute_allows_you: "时长式呼叫，使用多少就付多少. 账户中存点钱就可以开始, 使用过程中进行收费.",
+your_current_balance: "您当前的余额:",
+i_would_like_to_add: "我想增加",
+to_my_current_balance: "到我当前的余额",
+purchase: "购买",
+flat_rate_calling_allows_you: "租赁式呼叫，每个月只要支付固定的费用就可以了.",
+flat_rate_outbound_calling: "月租式外呼",
+allow_up_to: "允许至多",
+simultaneous_outbound_calls: "并发外呼",
+month_per_line: "每线每月",
+flat_rate_inbound_calling: "月租式呼入",
+simultaneous_inbound_calls_at: "并发呼入",
+if_you_dont_want_flat_rate: "如果你不想要月租，你的账户默认按分钟计费",
+save: "保存",
+add_credit: "(充值)",
+your_on_file_credit_card_will_immediately: "您做的任何改动都可能导致信用卡扣费. 如果您修改了定期业务, 将按计费周期按比例收费.<br/><br/>您确定要继续吗?",
+your_changes_have_been_saved: "您的修改已保存!",
+manage_your_credits_and_limits: "管理您的费用和呼叫限制."	
+};
