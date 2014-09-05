6133ba64031f37719c7afe8b9e48b8312aee8eb4
yumminova <info@yumminova.eu>
2014-09-05 09:19:19 +0200

    Updated Dutch translation and perhaps corrected a spell error in EN,but that maybe my poor english

---
+++ config/lang/nl.js
@@ -0,0 +1,70 @@
+window.translate['config'] = {
+	voip_label: "Hosted PBX",
+	voip_desc: "Beheer vmboxes, callflows ...",
+	cluster_label: "Cluster Manager",
+	cluster_desc: "Beheer Servers en Infrastructuur",
+	userportal_label: "Gebruikers Portal",
+	userportal_desc: "Laat de gebruiker zijn vm beheren ...",
+	accounts_label: "Accounts",
+	accounts_desc: "beheer uw sub-accounts",
+	developer_label: "Ontwikkelaar",
+	developer_desc: "Api Ontwikkelaar Tool",
+	pbxs_label: "PBX Connector",
+	pbxs_desc: "Beheer uw pbx-en",
+	numbers_label: "Nummer Manager",
+	numbers_desc: "beheer uw nummers",
+	there_were_errors: "Er is iets fout met het formulier, corrigeer dit aub!", //all errors on the form
+	OK: "OK",		//button OK confirm dialog
+	CANCEL: "Annuleren",	//button Cancel confirm dialog
+	CLOSE: "Sluiten",		//button Close alert dialog
+ 	hide: "Verberg",
+ 	show: "Toon",
+ 	hide_all: "Verberg alles",
+ 	show_all: "Toon alles",
+	please_confirm_title: "Bevestig aub",	//confirm title dialog
+	WARNING: "waarschuwing",
+			
+	/*-------------category actions--------*/
+	basic_cat: "Basis",
+	advanced_cat: "Geavanceerd",
+	call_forwarding_cat: "Doorschakelen",
+	caller_id_cat: "Nummerweergave",
+	call_recording_cat: "gespreksopname",
+	call_center_cat: "Call-Center",
+	hotdesking_cat: "Hotdesking",
+	time_of_day_cat: "Tijdsschakelingen",
+	/*-------------------------------------*/
+	advanced_menu_cat: "advanced",
+	
+	/*-------------jquery.dataTable--------*/
+	sProcessing: "Verwerken...",
+	sLengthMenu: "Toon _MENU_ items",
+	sZeroRecords: "Geen overeenkomstige gevonden",
+	sEmptyTable: "Geen data beschikbaar in de tabel",
+	sLoadingRecords: "Laden...",
+	sInfo: "Toon _START_ tot _END_ van _TOTAL_ items",
+	sInfoEmpty: "Toon 0 tot 0 van 0 items",
+	sInfoFiltered: "(Gefilterd uit _MAX_ totale items)",
+	sInfoPostFix: "",
+	sSearch: "Zoeken:",
+	sUrl: "",
+	sFirst: "Eerste",
+	sPrevious: "Vorige",
+	sNext: "Volgende",
+	sLast: "Laatste",
+	/*-------------------------------------*/
+	
+	alert_logout: "Uw huidige sessie loopt af en u wordt dadelijk uitgelogd vanwege geen activiteit. Beweeg de curson om ingelogd te blijven.",
+	charges_summary_title: "Kosten overzicht",
+	no_charges: 'Er zijn geen kosten voor deze actie.',
+	content_charges: "Hier een overzicht van de kosten per service in uw account:",
+	there_is_no: "Er is geen kostenpost voor deze actie: ",
+	you_will_pay: "U betaald € ",
+	one_time: " (eenmalig)-voor ",
+	service: "Service",
+	rate: "Tarief",
+	quantity: "Aantal",
+	discount: "Korting",
+	monthly_charges: "Maandelijkse Kosten",
+	press_OK_or_Cancel: "Klik ok om door te gaan, of anuleren om af te breken."
+};
