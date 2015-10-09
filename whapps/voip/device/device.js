e8144feb46e75fe2e4809b19b43a3c2ff657e196
Lucas Bussey <lucas@Lucass-MacBook-Pro.local>
2015-10-09 12:10:31 -0700

    Dynamic callflows with custom KVS

--- whapps/voip/device/device.js
+++ whapps/voip/device/device.js
@@ -1176,7 +1176,8 @@ winkstart.module('voip', 'device', {
                                     objects: {
                                         items: winkstart.sort(data.data),
                                         selected: node.getMetadata('id') || ''
-                                    }
+                                    },
+                                    route_var: node.getMetadata('var') || ''
                                 });
 
                                 if($('#device_selector option:selected', popup_html).val() == undefined) {
@@ -1193,6 +1194,11 @@ winkstart.module('voip', 'device', {
                                         node.setMetadata('id', _data.data.id || 'null');
                                         node.setMetadata('timeout', $('#parameter_input', popup_html).val());
                                         node.setMetadata('can_call_self', $('#device_can_call_self', popup_html).is(':checked'));
+                                        if($('#route_var', popup_html).val().length > 0) {
+                                            node.setMetadata('var', $('#route_var', popup_html).val());
+                                        } else {
+                                            node.deleteMetadata('var');
+                                        }
 
                                         node.caption = _data.data.name || '';
 
@@ -1200,11 +1206,20 @@ winkstart.module('voip', 'device', {
                                     });
                                 });
 
+                                $('#toggle_advanced', popup_html).click(function () {
+                                    $('#route_var_div', popup_html).toggle();
+                                });
+
                                 $('#add', popup_html).click(function() {
                                     node.setMetadata('id', $('#device_selector', popup_html).val());
                                     node.setMetadata('timeout', $('#parameter_input', popup_html).val());
                                     node.setMetadata('can_call_self', $('#device_can_call_self', popup_html).is(':checked'));
-
+                                    if($('#route_var', popup_html).val().length > 0) {
+                                        node.setMetadata('var', $('#route_var', popup_html).val());
+                                    } else {
+                                        node.deleteMetadata('var');
+                                    }
+                                        
                                     node.caption = $('#device_selector option:selected', popup_html).text();
 
                                     popup.dialog('close');
