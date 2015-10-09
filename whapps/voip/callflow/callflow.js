12d40cab2d9f6f9ca4ca9a32f219dc68c3e43096
Lucas Bussey <lucas@Lucass-MacBook-Pro.local>
2015-10-09 14:09:47 -0700

    Added button that will copy/duplicate an existing callflow and strip out assigned numbers.

--- whapps/voip/callflow/callflow.js
+++ whapps/voip/callflow/callflow.js
@@ -384,6 +384,15 @@ winkstart.module('voip', 'callflow', {
                 }
             });
 
+            $('.copy_icon', '#ws_cf_flow').click(function() {
+                delete(THIS.dataCallflow.id);
+                delete(THIS.dataCallflow.numbers);
+                THIS.flow.name = '';    
+                THIS.flow.numbers = [];
+                THIS.flow.id = undefined;
+                THIS.renderFlow();
+            });
+
             $('.buttons').append(buttons_html);
         },
 
@@ -506,9 +515,11 @@ winkstart.module('voip', 'callflow', {
             var THIS = this;
             if(pending_change) {
                 $('#pending_change', '#ws_callflow').show();
+                $('.copy_icon', '#ws_cf_flow').hide();
                 $('.save', '#ws_callflow').addClass('pulse-box');
             } else {
                 $('#pending_change', '#ws_callflow').hide();
+                $('.copy_icon', '#ws_cf_flow').show();
                 $('.save', '#ws_callflow').removeClass('pulse-box');
             }
         },
