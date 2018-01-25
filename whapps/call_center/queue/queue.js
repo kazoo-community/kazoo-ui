cdc7e77d7be1e6dad27501ffb278160441761e4b
Daniel Finke <danielfinke2011@gmail.com>
2018-01-25 01:44:54 +0000

    KAZUI-141: fix module action for Agent Pause callflow module

--- whapps/call_center/queue/queue.js
+++ whapps/call_center/queue/queue.js
@@ -1269,14 +1269,14 @@ winkstart.module('call_center', 'queue', {
                         );
                     }
                 },
-                'acdc_agent[action=pause]': {
+                'acdc_agent[action=paused]': {
                     name: _t('queue', 'agent_pause'),
                     icon: 'rightarrow',
                     category: _t('config', 'call_center_cat'),
                     module: 'acdc_agent',
                     tip: _t('queue', 'agent_pause_tip'),
                     data: {
-                        action: 'pause',
+                        action: 'paused',
                         timeout: '900',
                         presence_id: ''
                     },
