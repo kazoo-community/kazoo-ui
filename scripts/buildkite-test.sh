53da2d7f0c51c5adfb8fff500c13824caad09216
regner <shadowdf@gmail.com>
2019-11-25 16:17:24 -0800

    KAZUI-270 - Created a consul template file to replace per deployment configs
    Updated tests to also test the new config file as it ends in .tmpl and isn’t linted by default.
    
    Required consul keys:
    * common/ws-server
    * common/provisioner-url
    * common/api-url
    * kazoo-ui/sentry-dsn

--- scripts/buildkite-test.sh
+++ scripts/buildkite-test.sh
@@ -8,3 +8,8 @@ npm install
 echo "--- Run Full Test Suite"
 npm run test
 
+echo "--- Linting consul config template"
+DIFF_RESULT=$(git diff origin/develop -- config/config.js.tmpl)
+if [ "$DIFF_RESULT" != "" ]; then
+    node_modules/.bin/eslint config/config.js.tmpl
+fi
